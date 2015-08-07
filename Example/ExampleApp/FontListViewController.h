61c37caf57e927e41eb7b929274a602a60624045
yohei okada <yohei.states@akane.waseda.jp>
2015-08-07 16:24:49 +0900

    Replace view controller with FontListViewController

---
+++ Example/ExampleApp/FontListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FontListViewController.h
+//  GoogleFontsiOS
+//
+//  Created by Okada Yohei on 8/7/15.
+//  Copyright (c) 2015 yohei okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontListViewController : UITableViewController
+
+@end
---
+++ Example/ExampleApp/FontListViewController.m
@@ -0,0 +1,48 @@
+//
+//  FontListViewController.m
+//  GoogleFontsiOS
+//
+//  Created by Okada Yohei on 8/7/15.
+//  Copyright (c) 2015 yohei okada. All rights reserved.
+//
+
+#import "FontListViewController.h"
+
+@interface FontListViewController ()
+
+@end
+
+@implementation FontListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+@end
